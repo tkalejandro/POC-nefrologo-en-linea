aa50b846a25ebfeb2f6b5fccc063f59e5c1de5d7
J. Alejandro Coronado Cuesta <tk.jalejandro@gmail.com>
2022-12-03 12:13:13 +0100

    enums worked.

--- src/enums/SaludTools.ts
+++ src/enums/SaludTools.ts
@@ -11,17 +11,59 @@ export enum SaludToolsGender {
  * Type of Ids according SaludTools. National ID, National ForeignId
  */
 export enum SaludToolsDocumentType {
-    Cedula = 1,
-    CedulaExtranjeria = 2,
-    Passport = 3,
+    CedulaCiudadania = 1,
+    CedulaDeExtranjeria = 2,
+    CarneDiplomatico = 3,
+    Passaporte = 4,
+    RegistroCivil = 5,
+    TarjetDeIdentidad = 6,
+    AdultoSinIdentificar = 7,
+    MenorSinIdentificar = 8,
+    CertificadoDeNacidoVivo = 9,
+    Salvoconducto = 10,
+    PasaporteDeLaOnu = 11,
+    NIT = 12,
+    DocumentoNacionalDeIndentidad = 13,
+    PermisoEspecialDePermanencia = 14,
+    CedulaDeIdentidad = 15,
+    RegistroGeral = 16,
+    CedulaDeIdentidadCivil = 17,
+    DocumentoExtranjero = 18,
+    SinIdentificaion = 19,
+    ClaveUnicaDelRegistroDePoblacion = 20,
+    InsitutoNacionalElectoral = 21,
+    PermisoDeProteccionTemporal = 22,
+    RegistroNacionalDeContribuyentes = 23
 }
 
 /**
  * Type of Insurance
  */
 export enum SaludToolsEPS {
-    Prepagada = 1,
-    Privada = 2
+    UnidadServicioMedicoYOdontologicoDeEmpresasPublicasDeMedellin = 1,
+    FondoPasivoSocialFerrocarrilesNacionalesDeColombia = 2,
+    Aliansalud_EPS = 3,
+    SaludTotalEntidadPromotoraDeSaludDelRegimenContributivo = 4,
+    MedimasEpsContributivo = 5,
+    EPS_Sanitas = 6,
+    Compensar = 7,
+    ComfenalcoAntioquiaCajaDeCompensacionFamiliarComfenalcoAntioquia = 8,
+    SuraCompaniaSuramericanaDeServiciosDeSalud = 9,
+    ComfenalcoValle_EPS = 10,
+    SaludCoop_EPS = 11,
+    HumanaVivirArs = 12,
+    ColpatriaSaludColpatriaSA = 13,
+    CoomevaEPS_SA = 14,
+    EPSFamisanar_LTDA = 15,
+    ServicioOccidentalDeSaludSA_SOS = 16,
+    CruzBlanca_EPS_Contributivo = 17,
+    SolsaludEntidadPromotoraDeSaludDelRegimenContributivoYSubsidiadoSolSalud_EPS_SA = 18,
+    SaludVida_EPS = 19,
+    Nueva_EPS_Contributivo = 20
+
+    /**
+     * List continues until 121. Maybe to fetch this info.
+     */
 }
 
 /**
@@ -57,7 +99,17 @@ export enum SaludToolsModality {
  * State of the appointment
  */
 export enum SaludToolsStateAppointment {
-    Pending = "PENDING"
+    Completed = "EXECUTE",
+    Pending = "PENDING",
+    InRoom = "IN_ROOM",
+    OnGoing ="MEDICAL_CARE",
+    NoShow = "NOT_PRESENTEE",
+    Cancelado = "CANCELLED",
+    WaitPayment = "WAIT_PAYMENT",
+    InVirtualRoom = "IN_VIRTUAL_ROOM",
+    CancelledByDoctor = "CANCELLED_BY_DOCTOR",
+    Rescheduled = "RESCHEDULED",
+    Incomplete = "INCOMPLETE"
 }
 
 /**
