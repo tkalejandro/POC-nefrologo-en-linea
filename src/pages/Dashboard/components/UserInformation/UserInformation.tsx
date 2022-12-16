5774a21aa681971fabc3eb044a5a3c8e280fec76
Maximilian Faust <BsHr_Faust@web.de>
2022-12-16 17:03:07 +0100

    added 2 placeholder fields for future user information

--- src/pages/Dashboard/components/UserInformation/UserInformation.tsx
+++ src/pages/Dashboard/components/UserInformation/UserInformation.tsx
@@ -1,4 +1,4 @@
-import { Box, useTheme, styled, Paper, Grid, useMediaQuery, Typography } from '@mui/material'
+import { Box, useTheme, /* styled, Paper, */ Grid, useMediaQuery, Typography } from '@mui/material'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import React from 'react'
 
@@ -7,11 +7,11 @@ import React from 'react'
 const UserInformation = (): JSX.Element => {
 
     const theme = useTheme()
-    const Item = styled(Paper)({
+    /* const Item = styled(Paper)({
         padding: theme.spacing(1),
         textAlign: 'center',
         color: theme.palette.text.secondary,
-    });
+    }); */
 
     const isMd = useMediaQuery(theme.breakpoints.up('md'));
     return (
@@ -22,24 +22,33 @@ const UserInformation = (): JSX.Element => {
                 flexDirection: 'column',
                 alignItems: 'center',
             }}>
-                <Box>
-                    <AccountCircleIcon sx={{ fontSize: isMd ? 300 : 200, color: 'primary' }} />
-                    <Typography mt={2} variant={isMd ? 'h3' : 'h4'} color='primary'>
+                <Box sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}>
+                    <AccountCircleIcon sx={{ fontSize: isMd ? 250 : 200, color: 'primary' }} />
+                    <Typography mt={2} variant={isMd ? 'h3' : 'h4'}>
                         Trevor Phillips
                     </Typography>
                 </Box>
                 <Typography mt={4}>
+                    {/* available currently hardcoded */}
                     <Typography color='primary' variant='h5' component='span'>3 </Typography>
                     appointments available
                 </Typography>
             </Box>
-            <Grid container spacing={2}>
+            <Grid container spacing={2} mt={4}>
                 {/* User Profile Picture and name */}
-                <Grid item xs={12} lg={6}>
-                    <Item>xs=4</Item>
+                <Grid item xs={6} lg={6}>
+                    <Typography textAlign="center">
+                        Age
+                    </Typography>
                 </Grid>
-                <Grid item xs={12} lg={8}>
-                    <Item>xs=8</Item>
+                <Grid item xs={6} lg={6}>
+                    <Typography textAlign="center">
+                        Height
+                    </Typography>
                 </Grid>
             </Grid>
         </Box>
